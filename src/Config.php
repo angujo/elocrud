448a1f3d6fa5e49871a3273ef696ac00cf8d6ab4
bangujo <bangujo@kemri-wellcome.org>
2019-05-21 17:21:11 +0300

    config setup

---
+++ src/Config.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Angujo\Elocrud;
+
+
+class Config
+{
+    const CLASS_NAME = 'table';
+    const COLUMN_NAME = 'column';
+    const CONSTRAINT_NAME = 'constraint';
+
+    public static function relationFunctionName()
+    {
+
+    }
+}
--- src/Models/Method.php
+++ src/Models/Method.php
@@ -4,13 +4,110 @@
 namespace Angujo\Elocrud\Models;
 
 
+use Angujo\DBReader\Models\ForeignKey;
+
 class Method
 {
     private $comment;
     private $returns = true;
     private $name;
     private $run;
+    private $output;
     private $static = false;
+    private $access = 'public';
+    private static $me = [];
+    private static $c_name = '_default_';
+
+    protected function __construct($name)
+    {
+        $this->setName($name);
+        self::$me[self::$c_name][] = $this;
+    }
+
+    public static function init($name)
+    {
+        if (!is_string($name) || !is_numeric($name)) return;
+        self::$me[$name] = isset(self::$me[$name]) ? self::$me[$name] : [];
+        self::$c_name = $name;
+    }
+
+    public static function fromForeignKey(ForeignKey $foreignKey)
+    {
+
+    }
+
+    /**
+     * @return string
+     */
+    public function getAccess(): string
+    {
+        return $this->access;
+    }
+
+    /**
+     * @param string $access
+     * @return Method
+     */
+    public function setAccess(string $access): Method
+    {
+        if (!in_array($access, ['public', 'protected', 'private'])) $access = 'public';
+        $this->access = $access;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOutput()
+    {
+        return $this->output;
+    }
+
+    /**
+     * @param mixed $output
+     * @return Method
+     */
+    public function setOutput($output)
+    {
+        $this->output = $output;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isReturns(): bool
+    {
+        return $this->returns;
+    }
+
+    /**
+     * @param bool $returns
+     * @return Method
+     */
+    public function setReturns(bool $returns): Method
+    {
+        $this->returns = $returns;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isStatic(): bool
+    {
+        return $this->static;
+    }
+
+    /**
+     * @param bool $static
+     * @return Method
+     */
+    public function setStatic(bool $static): Method
+    {
+        $this->static = $static;
+        return $this;
+    }
 
     /**
      * @return mixed
@@ -74,5 +171,4 @@ class Method
         $this->run[] = $run;
         return $this;
     }
-
 }
