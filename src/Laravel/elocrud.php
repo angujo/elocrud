b6497af2661a0b6618e5d2344e8858151baa25d5
bangujo <bangujo@kemri-wellcome.org>
2019-05-30 14:18:56 +0300

    just config

--- src/Laravel/elocrud.php
+++ src/Laravel/elocrud.php
@@ -6,35 +6,41 @@
  * Time: 12:29 PM
  */
 
+/*
+ * This is the configuration page for custom setup.
+ * Worth noting that this library relies on directory levels for namespace
+ * Accessible models will be directly placed on "base_dir"
+ * Any subsequent extension or supporting directories will be set deeper into "base_dir"
+ */
 return [
     /*
      * Column names that are used for soft delete.
      * If different naming across tables, add them here.
      * NOTE: No two names should be on same table.
      */
-    'soft_delete_columns' => ['deleted_at'],
+    'soft_delete_columns'     => ['deleted_at'],
 
     /*
      * Tables to be excluded from model generation
      */
-    'excluded_tables' => ['migrations'],
+    'excluded_tables'         => ['migrations'],
     /*
      *Tables to be run ONLY
      * The reset will be excluded
      */
-    'only_tables' => [],
+    'only_tables'             => [],
     /*
      * Column names to mark as create columns
      * If different naming across tables, add them here.
      * NOTE: No two names should be on same table.
      */
-    'create_columns' => ['created_at'],
+    'create_columns'          => ['created_at'],
     /*
      * Columns to be used as update
      * If different naming across tables, add them here.
      * NOTE: No two names should be on same table.
      */
-    'update_columns' => ['updated_at'],
+    'update_columns'          => ['updated_at'],
     /*
      * Prefix used to mark relationship column names
      * Depends on naming conventions
@@ -49,24 +55,24 @@ return [
      * It is recommended to have a prefix/suffix/both to separate the column value from the relation,
      * otherwise, might break the models
      */
-    'relation_remove_prx' => 'fk',
+    'relation_remove_prx'     => 'fk',
     /*
      * @see doc for 'relation_remove_prx' above
      */
-    'relation_remove_sfx' => 'id',
+    'relation_remove_sfx'     => 'id',
     /*
      * Class to be used for each and every generated model
      * Ensure it is or extends \Illuminate\Database\Eloquent\Model::class
      */
-    'model_class' => \Illuminate\Database\Eloquent\Model::class,
+    'model_class'             => \Illuminate\Database\Eloquent\Model::class,
     /*
      * Directory path to put the models
      */
-    'base_dir' => app_path('Models'),
+    'base_dir'                => app_path('Models'),
     /*
      * Enable composite keys in laravel
      */
-    'composite_keys' => true,
+    'composite_keys'          => true,
     /*
      * Name of class to be used in customizing Eloquent to accommodate composite keys.
      */
@@ -74,16 +80,38 @@ return [
     /*
      * Create abstract classes to act as BASE Class for teh tables
      */
-    'base_abstract' => true,
+    'base_abstract'           => true,
     /*
      * Namespace for the models
      */
-    'namespace' => 'App\Models',
+    'namespace'               => 'App\Models',
+    /*
+     * Pivoting allows Laravel's hasManyThrough to be extended.
+     * Currently, we extend upto 3 levels deep
+     * NOTE: This relies on foreign keys, so will not work if not set on tables.
+     * Any value greater than 3 will be considered as 3 and any less than zero converted to 0
+     */
+    'pivot_level'             => 0,
+    /*
+     * This is the nested namespace from the "base_dir" above
+     * to be used for pivot tables
+     */
+    'pivot_extension_name'    => 'Pivot',
+    /*
+     * @link https://laravel.com/docs/5.6/eloquent-relationships#polymorphic-relations
+     * Add polymorphic tables as well.
+     * To set this up column naming should be as described in laravel
+     * On the "_type" list all tables to be referenced on the column's comments
+     * While on the "_id" list corresponding column's comments (optional)
+     * if the "_id" column lacks a comment, primaryKey of the table will be assumed
+     * All listing should be separated by a comma
+     */
+    'polymorph'               => true,
     /*
      * Type Casting for properties and database values.
      * You can cast using a column name or data type.
      * To cast data type e.g. tinyint(1) to be boolean,
      * start with "type:" followed by the type i.e. "type:tinyint(1)"=>'boolean'
      */
-    'type_casts' => ['type:tinyint(1)' => 'boolean', '%_json' => 'array', '%_array' => 'array', 'is_%' => 'boolean','type:date'=>'date:Y-m-d','type:datetime'=>'datetime:Y-m-d H:i:s'],
+    'type_casts'              => ['type:tinyint(1)' => 'boolean', '%_json' => 'array', '%_array' => 'array', 'is_%' => 'boolean', 'type:date' => 'date:Y-m-d', 'type:datetime' => 'datetime:Y-m-d H:i:s'],
 ];
